a05d8007ae65fa116d7196fc046cd7db7ef2db9a
Germano Macieira <germanomacieira@hotmail.com>
2025-09-15 21:44:33 -0300

    Função para mostrar a lista de amigos na tela

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-// Lista onde vamos guardar os nomes dos amigos
+
 let amigos = [];
 
-// Função para adicionar um amigo à lista
 function adicionarAmigo() {
-    // Pega o valor digitado no input
     let input = document.getElementById("amigo");
-    let nome = input.value.trim(); // remove espaços extras
+    let nome = input.value.trim();
 
-    // Verifica se o campo não está vazio
     if (nome === "") {
         alert("Por favor, digite um nome!");
         return;
     }
 
-    // Adiciona o nome no array de amigos
     amigos.push(nome);
 
-    // Atualiza a lista de amigos na tela
     mostrarAmigos();
 
-    // Limpa o campo de input
     input.value = "";
+}
+
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
 }
